6a80c22f42ea8482a7db4cd9e0c73f9e17d9ac4b
unknown <185206@BME.HU>
2024-10-09 09:36:24 +0200

    vegso

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -31,7 +31,9 @@ public class TorpedoStore {
   }
 
   public boolean fire(int numberOfTorpedos){
-
+    if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
+      throw new IllegalArgumentException("numberOfTorpedos");
+    }
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
